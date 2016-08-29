16db616af79aa3fcc7e4f84490ce37d0e2fc146d
Daniel Fernes <daniel.fernes@gmail.com>
2016-08-29 14:32:33 +0200

    Tweaks and small changes
    Mandatory changes in results and crowd actions needed to work with Unity
    project

--- CrowdSimSetupWizard/WizardWindow.xaml.cs
+++ CrowdSimSetupWizard/WizardWindow.xaml.cs
@@ -16,23 +16,25 @@ namespace CrowdSimSetupWizard
     /// </summary>
     public partial class WizardWindow : Window
     { 
-        public struct ConfigData
+        private struct ConfigData
         {
+            //Scene:
             public string SceneName;
             public int DayTime;
             public int WeatherConditions;
+            //Crowd
             public string Models;
             public int MaxPeople;
+            public string CrowdActions;
+            //Scenario
             public bool Tracking;
             public string ScenarioFilePath;
             public int Length;
             public int Repeats;
             public int Instances;
+            //Results
             public string ResultsPath;
-            public string VisualFileExtension;
-            public int VisualResultsType;
-            public string AnnotationsFileExtension;
-            public int AnnotationsType;
+            public bool BoundingBoxes;
         }
 
         private static ConfigData _data;
@@ -40,6 +42,7 @@ namespace CrowdSimSetupWizard
         private List<ModelFile> _models;
         private string _path;
         private StringBuilder _modelsFilter;
+        private StringBuilder _actionsFilter;
 
         public WizardWindow()
         {
@@ -47,84 +50,38 @@ namespace CrowdSimSetupWizard
             InitializeComponent();
 
             _data.Tracking = false;
-            _data.VisualFileExtension = "|PNG";
-            _data.AnnotationsFileExtension = "|TXT";
-            _data.AnnotationsType = 1;
+            _data.BoundingBoxes = true;
         }
 
-        private void Choose_Scenario_Button_Click(object sender, RoutedEventArgs e)
+        private void Scene_List_Checked(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-
-            dlg.Filter = "XML Files (*.xml)|*.xml";
-            Nullable<bool> result = dlg.ShowDialog();
-
-            if (result == true)
+            if (sender.Equals(Square))
             {
-                string fileName = dlg.FileName;
-                _data.ScenarioFilePath = fileName;
-                Scenario_File_Path.Text = fileName;
-                File_Contents.Text = File.ReadAllText(fileName);
-                ScenarioPage.CanSelectNextPage = true;
+                _data.SceneName = "Crowd Simulator Prototype";
+            }
+            else if (sender.Equals(Obstacles))
+            {
+                _data.SceneName = "Scene with obstacles";
+            }
+            else if (sender.Equals(City))
+            {
+                _data.SceneName = "City like scene";
             }
         }
 
-        private void Length_Value_Picker_Spinned(object sender, Xceed.Wpf.Toolkit.SpinEventArgs e)
+        private void Day_Time_Checked(object sender, RoutedEventArgs e)
         {
-            _data.Length = (int)Length_Value_Picker.Value;
-        }
-
-        private void Tracking_CheckBox_Click(object sender, RoutedEventArgs e)
-        {
-            if ((bool)Tracking_CheckBox.IsChecked)
+            if (sender.Equals(Morning))
             {
-                _data.Tracking = true;
-                Choose_Scenario_Button.IsEnabled = false;
-                Instances_Value_Picker.IsReadOnly = true;
-                Length_Value_Picker.IsReadOnly = false;
-                Repeats_Header.Text = "Repeat session:";
-                ScenarioPage.CanSelectNextPage = true;
+                _data.DayTime = 1;
             }
-            else
+            else if (sender.Equals(Noon))
             {
-                _data.Tracking = false;
-                Choose_Scenario_Button.IsEnabled = true;
-                Instances_Value_Picker.IsReadOnly = false;
-                Length_Value_Picker.IsReadOnly = true;
-                Repeats_Header.Text = "Play scenario:";
-                if (Scenario_File_Path.Text != "")
-                {
-                    ScenarioPage.CanSelectNextPage = true;
-                }
-                else
-                {
-                    ScenarioPage.CanSelectNextPage = false;
-                }
+                _data.DayTime = 2;
             }
-        }
-
-        private void Value_Picker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            if (sender.Equals(Length_Value_Picker))
+            else if (sender.Equals(Afternoon))
             {
-                _data.Length = (int)Length_Value_Picker.Value;
-            }
-            else if (sender.Equals(Instances_Value_Picker))
-            {
-                _data.Instances = (int)Instances_Value_Picker.Value;
-            }
-            else if (sender.Equals(Repeats_Value_Picker))
-            {
-                _data.Repeats = (int)Repeats_Value_Picker.Value;
-            }
-        }
-
-        private void Crowd_Size_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            _data.MaxPeople = (int)Crowd_Size_Slider.Value;
-            if (Crowd_Number_Presentation != null)
-            {
-                Crowd_Number_Presentation.Text = _data.MaxPeople.ToString();
+                _data.DayTime = 3;
             }
         }
 
@@ -152,102 +109,36 @@ namespace CrowdSimSetupWizard
             }
         }
 
-        private void Day_Time_Checked(object sender, RoutedEventArgs e)
+        private void Crowd_Size_Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            if (sender.Equals(Morning))
+            _data.MaxPeople = (int)Crowd_Size_Slider.Value;
+            if (Crowd_Number_Presentation != null)
             {
-                _data.DayTime = 1;
-            }
-            else if (sender.Equals(Noon))
-            {
-                _data.DayTime = 2;
-            }
-            else if (sender.Equals(Afternoon))
-            {
-                _data.DayTime = 3;
+                Crowd_Number_Presentation.Text = _data.MaxPeople.ToString();
             }
         }
 
-        private void Scene_List_Checked(object sender, RoutedEventArgs e)
+        private void Models_List_Initialized(object sender, EventArgs e)
         {
-            if (sender.Equals(Square))
+            GetModelsList();
+        }
+
+
+
+        private void Models_CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.CheckBox model = sender as System.Windows.Controls.CheckBox;
+            if ((bool)model.IsChecked)
             {
-                _data.SceneName = "Crowd Simulator Prototype";
+                _modelsFilter.Append("|" + model.Content);
             }
-            else if (sender.Equals(Obstacles))
+            else
             {
-                _data.SceneName = "Scene with obstacles";
-            }
-            else if (sender.Equals(City))
-            {
-                _data.SceneName = "City like scene";
+                _modelsFilter.Replace("|" + model.Content, string.Empty);
             }
         }
 
-        private void Results_Type_Checked(object sender, RoutedEventArgs e)
-        {
-            if (sender.Equals(Type_Raw))
-            {
-                _data.VisualResultsType = 1;
-            }
-            else if (sender.Equals(Type_Sequences))
-            {
-                _data.VisualResultsType = 2;
-            }
-            else if (sender.Equals(Type_Sequences_Boxes))
-            {
-                _data.VisualResultsType = 3;
-            }
-        }
-
-        private void Visual_File_Type_Click(object sender, RoutedEventArgs e)
-        {
-            //NO YET IMPLEMENTED IN UNITY PROJECT
-        }
-
-        private void Annotations_File_Type_Click(object sender, RoutedEventArgs e)
-        {
-            //NO YET IMPLEMENTED IN UNITY PROJECT
-        }
-
-        private void Choose_Results_Button_Click(object sender, RoutedEventArgs e)
-        {
-            var dlg = new FolderBrowserDialog();
-            DialogResult result = dlg.ShowDialog();
-            if(result == System.Windows.Forms.DialogResult.OK)
-            {
-                _data.ResultsPath = dlg.SelectedPath;
-                Results_Path.Text = _data.ResultsPath;
-                ResultsPage.CanFinish = true;
-            }
-        }
-
-        private void Animations_List_Initialized(object sender, EventArgs e)
-        {
-            GetAnimationsList();
-        }
-
-        private void GetAnimationsList()
-        {
-            if (_animations != null)
-            {
-                _animations.Clear();
-            }
-            string animationsPath = _path + "Unity-CrowdSim-Prototype\\Assets\\Resources\\Animations\\";
-            string[] animationFiles = Directory.GetFiles(animationsPath, "*.fbx", SearchOption.AllDirectories);
-            for (int i = 0; i < animationFiles.Length; i++)
-            {
-                animationFiles[i] = Path.GetFileNameWithoutExtension(animationFiles[i]);
-            }
-            _animations = new List<AnimationFile>();
-            foreach (string file in animationFiles)
-            {
-                _animations.Add(new AnimationFile() { FileName = file });
-            }
-            Animations_List.ItemsSource = _animations;
-        }
-
-        private void Add_Action_Button_Click(object sender, RoutedEventArgs e)
+        private void Add_Model_Button_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
@@ -256,15 +147,10 @@ namespace CrowdSimSetupWizard
 
             if (result == true)
             {
-                string destPath = _path + "Unity-CrowdSim-Prototype\\Assets\\Resources\\Animations\\" + Path.GetFileName(dlg.FileName);
-                File.Copy(dlg.FileName, destPath, true);
-                GetAnimationsList();
+                string destPath = string.Format("{0}Unity-CrowdSim-Prototype\\Assets\\Characters\\{1}\\{2}", _path, Path.GetFileNameWithoutExtension(dlg.FileName), Path.GetFileName(dlg.FileName));
+                Directory.CreateDirectory(Path.GetDirectoryName(destPath));
+                GetModelsList();
             }
-        }
-
-        private void Models_List_Initialized(object sender, EventArgs e)
-        {
-            GetModelsList();
         }
 
         private void GetModelsList()
@@ -288,7 +174,46 @@ namespace CrowdSimSetupWizard
             Models_List.ItemsSource = _models;
         }
 
-        private void Add_Model_Button_Click(object sender, RoutedEventArgs e)
+        private void Animations_List_Initialized(object sender, EventArgs e)
+        {
+            GetAnimationsList();
+        }
+
+        private void GetAnimationsList()
+        {
+            if (_animations != null)
+            {
+                _animations.Clear();
+            }
+            _actionsFilter = new StringBuilder();  
+            string animationsPath = _path + "Unity-CrowdSim-Prototype\\Assets\\Resources\\Animations\\";
+            string[] animationFiles = Directory.GetFiles(animationsPath, "*.fbx", SearchOption.AllDirectories);
+            for (int i = 0; i < animationFiles.Length; i++)
+            {
+                animationFiles[i] = Path.GetFileNameWithoutExtension(animationFiles[i]);
+            }
+            _animations = new List<AnimationFile>();
+            foreach (string file in animationFiles)
+            {
+                _animations.Add(new AnimationFile() { FileName = file });
+            }
+            Actions_List.ItemsSource = _animations;
+        }
+
+        private void Actions_CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.CheckBox action = sender as System.Windows.Controls.CheckBox;
+            if ((bool)action.IsChecked)
+            {
+                _actionsFilter.Append("|" + action.Content);
+            }
+            else
+            {
+                _actionsFilter.Replace("|" + action.Content, string.Empty);
+            }
+        }
+
+        private void Add_Action_Button_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
@@ -297,23 +222,101 @@ namespace CrowdSimSetupWizard
 
             if (result == true)
             {
-                string destPath = string.Format("{0}Unity-CrowdSim-Prototype\\Assets\\Characters\\{1}\\{2}", _path, Path.GetFileNameWithoutExtension(dlg.FileName),Path.GetFileName(dlg.FileName));
-                Directory.CreateDirectory(Path.GetDirectoryName(destPath));
-                GetModelsList();
+                string destPath = _path + "Unity-CrowdSim-Prototype\\Assets\\Resources\\Animations\\" + Path.GetFileName(dlg.FileName);
+                File.Copy(dlg.FileName, destPath, true);
+                GetAnimationsList();
             }
         }
 
-        private void Models_CheckBox_Click(object sender, RoutedEventArgs e)
+        private void Choose_Scenario_Button_Click(object sender, RoutedEventArgs e)
         {
-            System.Windows.Controls.CheckBox model = sender as System.Windows.Controls.CheckBox;
-            if ((bool)model.IsChecked)
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.Filter = "XML Files (*.xml)|*.xml";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
             {
-                _modelsFilter.Append("|" + model.Content);
+                string fileName = dlg.FileName;
+                _data.ScenarioFilePath = fileName;
+                Scenario_File_Path.Text = fileName;
+                File_Contents.Text = File.ReadAllText(fileName);
+                ScenarioPage.CanSelectNextPage = true;
+            }
+        }
+
+        private void Value_Picker_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (sender.Equals(Length_Value_Picker))
+            {
+                _data.Length = (int)Length_Value_Picker.Value;
+            }
+            else if (sender.Equals(Instances_Value_Picker))
+            {
+                _data.Instances = (int)Instances_Value_Picker.Value;
+            }
+            else if (sender.Equals(Repeats_Value_Picker))
+            {
+                _data.Repeats = (int)Repeats_Value_Picker.Value;
+            }
+        }
+
+        private void Tracking_CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)Tracking_CheckBox.IsChecked)
+            {
+                _data.Tracking = true;
+                Choose_Scenario_Button.IsEnabled = false;
+                Instances_Value_Picker.IsReadOnly = true;
+                Length_Value_Picker.IsReadOnly = false;
+                Repeats_Header.Text = "Repeat tracking session:";
+                ScenarioPage.CanSelectNextPage = true;
             }
             else
             {
-                _modelsFilter.Replace("|" + model.Content, string.Empty);
+                _data.Tracking = false;
+                Choose_Scenario_Button.IsEnabled = true;
+                Instances_Value_Picker.IsReadOnly = false;
+                Length_Value_Picker.IsReadOnly = true;
+                Repeats_Header.Text = "Repeat scenario:";
+                if (Scenario_File_Path.Text != "")
+                {
+                    ScenarioPage.CanSelectNextPage = true;
+                }
+                else
+                {
+                    ScenarioPage.CanSelectNextPage = false;
+                }
             }
+        }
+
+        private void Choose_Results_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog();
+            DialogResult result = dlg.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                _data.ResultsPath = dlg.SelectedPath;
+                Results_Path.Text = _data.ResultsPath;
+                ResultsPage.CanFinish = true;
+            }
+        }
+
+        private void Results_Type_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals(Type_Sequences))
+            {
+                _data.BoundingBoxes = false;
+            }
+            else if (sender.Equals(Type_Sequences_Boxes))
+            {
+                _data.BoundingBoxes = true;
+            }
+        }
+
+        private void Visual_File_Type_Click(object sender, RoutedEventArgs e)
+        {
+            //NO YET IMPLEMENTED IN UNITY PROJECT
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -322,6 +325,7 @@ namespace CrowdSimSetupWizard
             if (wasCodeClosed)
             {
                 _data.Models = _modelsFilter.ToString();
+                _data.CrowdActions = _actionsFilter.ToString();
                 XmlDocument config = new XmlDocument();
                 config.Load(_path + "Unity-CrowdSim-Prototype\\Assets\\config.xml");
                 XmlElement configElement = config.DocumentElement;
@@ -337,6 +341,7 @@ namespace CrowdSimSetupWizard
                     {
                         configElement.ChildNodes[i].Attributes[0].Value = _data.Models;
                         configElement.ChildNodes[i].Attributes[1].Value = _data.MaxPeople.ToString();
+                        configElement.ChildNodes[i].Attributes[2].Value = _data.CrowdActions.ToString();
                     }
                     else if (configElement.ChildNodes[i].Name == "simulation")
                     {
@@ -349,10 +354,7 @@ namespace CrowdSimSetupWizard
                     else if (configElement.ChildNodes[i].Name == "results")
                     {
                         configElement.ChildNodes[i].Attributes[0].Value = _data.ResultsPath.ToString();
-                        configElement.ChildNodes[i].Attributes[1].Value = _data.VisualFileExtension.ToString();
-                        configElement.ChildNodes[i].Attributes[2].Value = _data.VisualResultsType.ToString();
-                        configElement.ChildNodes[i].Attributes[3].Value = _data.AnnotationsFileExtension.ToString();
-                        configElement.ChildNodes[i].Attributes[4].Value = _data.AnnotationsType.ToString();
+                        configElement.ChildNodes[i].Attributes[1].Value = _data.BoundingBoxes.ToString();
                     }
                 }
                 config.Save(_path + "Unity-CrowdSim-Prototype\\Assets\\config.xml");
